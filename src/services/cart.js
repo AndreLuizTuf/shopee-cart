a3c6de308309526e3de5805e218904d2d934c3f1
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-20 14:51:49 -0300

    Assinaturas Cart.js

---
+++ src/services/cart.js
@@ -0,0 +1,14 @@
+// quais açoes meu carrinho pode fazer
+
+//CASOS DE USO
+// ✅ -> adicionar item no carrinho
+async function addItem(userCart) {}
+
+// -> deletar item do carrinho
+async function deleteItem(userCart, name) {}
+
+// -> ✅ remover um item - diminui um item
+async function removeItem(userCart, idenx) {}
+
+// ✅ -> calcular o total do carrinho
+async function calculateTotal(userCart) {}
