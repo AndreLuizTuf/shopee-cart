a2c3dc99b817155c52136ef65bee361d8925450e
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-20 15:27:06 -0300

    Cart Add e Cart Subtotal

--- src/services/cart.js
+++ src/services/cart.js
@@ -2,7 +2,14 @@
 
 //CASOS DE USO
 // ✅ -> adicionar item no carrinho
-async function addItem(userCart) {}
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
+
+// ✅ -> calcular o total do carrinho
+async function calculateTotal(userCart) {
+    return userCart.reduce((total, item) => total + item.subtotal(), 0);
+}
 
 // -> deletar item do carrinho
 async function deleteItem(userCart, name) {}
@@ -10,5 +17,12 @@ async function deleteItem(userCart, name) {}
 // -> ✅ remover um item - diminui um item
 async function removeItem(userCart, idenx) {}
 
-// ✅ -> calcular o total do carrinho
-async function calculateTotal(userCart) {}
+
+export {
+    addItem,
+    calculateTotal,
+    deleteItem,
+    removeItem
+}
+
+
